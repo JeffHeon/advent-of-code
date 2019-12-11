3044548a7d8f4df598485a90e617df715be906e5
Jean-François Héon <jfheon@gmail.com>
2019-12-10 21:26:33 -0500

    day 2 star 2

---
+++ 2019/day2/puzzle2.kts
@@ -0,0 +1,65 @@
+package day2
+
+import java.io.File
+
+class IntcodeComputer {
+    private var memory = mutableListOf<Int>()
+    private fun dereference(reference: Int) = memory[reference]
+
+    private fun processOperation(
+        position: Int,
+        operation: (Int, Int) -> Int
+    ) {
+        val parameter1 = memory[dereference(position + 1)]
+        val b = memory[dereference(position + 2)]
+        memory[dereference(position + 3)] = operation(parameter1, b)
+    }
+
+    fun loadProgram(program: List<Int>) {
+        memory = program.toMutableList()
+    }
+
+    fun run(): Int {
+        var instructionPointer = 0
+        loop@ while (instructionPointer < memory.size) {
+            val opCode = memory[instructionPointer]
+            when (opCode) {
+                99 -> break@loop
+                1 -> processOperation(instructionPointer, Integer::sum)
+                2 -> processOperation(instructionPointer) { a, b -> a * b }
+            }
+            instructionPointer += 4
+        }
+        return memory[0]
+    }
+
+    fun init(noun: Int, verb: Int) {
+        memory[1] = noun
+        memory[2] = verb
+    }
+}
+
+val program: List<Int> =
+    File("input").readLines()
+        .first()
+        .split(",")
+        .map(String::toInt)
+
+// Color Computer
+val myCoCo = IntcodeComputer()
+val targetOutput = 19690720
+masterControlProgram@
+for (noun in 0..99) {
+    for (verb in 0..99) {
+        myCoCo.loadProgram(program)
+        myCoCo.init(noun, verb)
+        val output = myCoCo.run()
+        if (output == targetOutput) {
+            println("Noun $noun, Verb $verb")
+            println("100 * noun + verb is ${100 * noun + verb}")
+            break@masterControlProgram
+        }
+    }
+}
+
+println("eol")
