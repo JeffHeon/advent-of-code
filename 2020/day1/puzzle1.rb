5b07f9eb9a3405982bcf2ac390e6761ebba6973b
Jean-François Héon <jfheon@gmail.com>
2020-12-02 20:05:42 -0500

    2020 Day1 Puzzle 1

---
+++ 2020/day1/puzzle1.rb
@@ -0,0 +1,19 @@
+raw_input = File.read("input").split
+
+input=raw_input.map { |s| s.to_i }
+
+numbers2020 = []
+last_index = input.length - 1
+for i in 0..last_index do
+    for j in i..last_index do
+        if input[i] + input[j] == 2020
+            numbers2020 = [input[i], input[j]]
+        end    
+    end    
+end
+
+if numbers2020.empty?
+    puts "No solution found."
+else
+    puts numbers2020[0] * numbers2020[1]    
+end
