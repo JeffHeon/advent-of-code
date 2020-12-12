060fe0b7dc6c2c491b89f2e2e89c7ce23a0a1d22
Jean-François Héon <jfheon@gmail.com>
2020-12-12 03:13:15 +0000

    day4 puzzle 1

--- 2020/day4/puzzle4.rb
+++ 2020/day4/puzzle4.rb
@@ -5,10 +5,26 @@ chunks = lines.chunk { |line| !line.empty? }
 
 password_chunks_keypair = chunks.select { |chunk| chunk.first }
 
-passwords_with_fields = password_chunks_keypair.map do |chunk|
+passports_with_fields = password_chunks_keypair.map do |chunk|
   chunk[1]
     .map { |chunk_line| chunk_line.split }
     .flatten
 end
 
-print passwords_with_fields
+Required_fields =
+  ['byr', # (Birth Year)
+   'iyr', # (Issue Year)
+   'eyr', # (Expiration Year)
+   'hgt', # (Height)
+   'hcl', # (Hair Color)
+   'ecl', # (Eye Color)
+   'pid'].freeze # (Passport ID)
+# "cid" #(Country ID) optional for now
+
+def valid?(passport)
+  Required_fields.all? do |field_id|
+    passport.any? { |field| field.start_with? field_id }
+  end
+end
+
+puts passports_with_fields.count { |passport| valid? passport }
