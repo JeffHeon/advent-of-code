f19517e86674b975262fc11f67e82ddca1c17e98
Jean-François Héon <jfheon@gmail.com>
2020-12-12 02:55:51 +0000

    Day 4 wip

--- 2020/day4/puzzle4.rb
+++ 2020/day4/puzzle4.rb
@@ -3,6 +3,12 @@ lines = File.readlines('./2020/day4/input').map(&:chomp)
 
 chunks = lines.chunk { |line| !line.empty? }
 
-password_chunks = chunks.select { |chunk| chunk.first }
+password_chunks_keypair = chunks.select { |chunk| chunk.first }
 
-puts password_chunks.first
+passwords_with_fields = password_chunks_keypair.map do |chunk|
+  chunk[1]
+    .map { |chunk_line| chunk_line.split }
+    .flatten
+end
+
+print passwords_with_fields
